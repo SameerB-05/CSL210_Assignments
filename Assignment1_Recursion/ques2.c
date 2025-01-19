9ed236a93d955db5a4418c4c166c3a343eabaf82
SameerB-05 <sameerbadami05@gmail.com>
2025-01-19 19:32:32 +0530

    Replaced ques2.c with the updated version

--- Assignment1_Recursion/ques2.c
+++ Assignment1_Recursion/ques2.c
@@ -9,28 +9,18 @@
 #include <limits.h>
 
 
-int fibonacci_call_count(int n)
+int fibonacci_call_count(int n, int *calls) 
 {
-    if(n==0 || n==1)
+    (*calls)++;
+
+    if (n <= 0)
+        return -1;
+    if (n == 1)
+        return 0;
+    if (n == 2)
         return 1;
-    
-    int left = fibonacci_call_count(n-1);
-    int right = fibonacci_call_count(n-2);
-    return left + right;
-}
 
-
-void fibonacci(int n, int a, int b) {
-    // Base case: If n reaches 0, return
-    if (n == 0) {
-        return;
-    }
-    
-    // Print the current Fibonacci number
-    printf("%d ", a);
-    
-    // Recursive call for next Fibonacci numbers
-    fibonacci(n - 1, b, a + b);
+    return fibonacci_call_count(n - 1, calls) + fibonacci_call_count(n - 2, calls);
 }
 
 
@@ -40,8 +30,9 @@ int main()
     printf("Enter n (natural number): ");
     scanf("%d", &n);
 
-    printf("Number of calls to fibbonaci function for n=%d is: %d\n", n, fibonacci_call_count(n));
-    printf("Fibonacci series up to %d terms: ", n);
-    fibonacci(n, 0, 1);
+    int calls = 0;
+    fibonacci_call_count(n, &calls);
+    
+    printf("Number of calls to fibbonaci function for n=%d is: %d\n", n, calls);
     return 0;
 }
